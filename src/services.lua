31c8cd59811272230dba343e23659799080db03a
ocawesome101 <ocawesome101@gmail.com>
2021-04-02 18:17:25 -0400

    some of a `sv' api now

--- src/services.lua
+++ src/services.lua
@@ -9,12 +9,32 @@ do
   local process = require("process")
   
   function sv.up(svc)
+    if running[svc] then
+      return true
+    end
+    local path = config[svc] and config[svc].file or
+      string.format("%s.lua", svc)
+
   end
   
   function sv.down(svc)
+    if not running[svc] then
+      return true
+    end
+    local ok, err = process.kill(running[svc])
+    if not ok then
+      return nil, err
+    end
+    running[svc] = nil
+    return true
   end
   
   function sv.list()
+    return setmetatable({}, {
+      __index = running,
+      __pairs = running,
+      __ipairs = running
+    })
   end
   
   function sv.msg()
